3e24e56c6e5483a2932a364fd70ea41943d6eee8
uni.kit <uni.ai.consalting@gmail.com>
2026-03-07 14:40:07 +0400

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -50,7 +50,7 @@ export function Header() {
                 <img
                   src="/lkz-site/images/common/logo.svg"
                   alt="Логотип"
-                  cclassName="relative -top-[2px] block h-auto max-h-[36px] w-auto max-w-[148px] object-contain"
+                  className="relative -top-[2px] block h-auto max-h-[36px] w-auto max-w-[148px] object-contain"
                 />
               </Link>
 
