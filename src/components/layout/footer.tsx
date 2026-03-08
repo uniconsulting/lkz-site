1efa38e16de5e5a855ce06c52b88c557a55e4178
uni.kit <uni.ai.consalting@gmail.com>
2026-03-08 19:44:00 +0400

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -115,7 +115,7 @@ export function Footer() {
               <img
                 src={`${basePath}/images/common/logo-dark.svg`}
                 alt="Логотип"
-                className="footer-logo-on-dark block h-auto w-auto max-h-[156px] max-w-[290px] object-contain"
+                className="footer-logo-on-dark block h-auto w-auto max-h-[116px] max-w-[250px] object-contain"
               />
 
               <img
