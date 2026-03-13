239969969c5c6b38a7740cd9c9bcf287c152ad26
uni.kit <uni.ai.consalting@gmail.com>
2026-03-13 10:53:33 +0400

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -497,7 +497,7 @@ export function Hero() {
   useEffect(() => {
     const intervalId = window.setInterval(() => {
       setBannerIndex((prev) => (prev + 1) % heroBanners.length);
-    }, 10000);
+    }, 17000);
 
     return () => window.clearInterval(intervalId);
   }, []);
